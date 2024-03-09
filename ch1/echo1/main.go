a8492a811e8bb5f47c9d8f8f396d2e268876bc42
fqqk <fqqk.work68@gmail.com>
2024-03-09 22:55:49 +0900

    go zero value

--- ch1/echo1/main.go
+++ ch1/echo1/main.go
@@ -13,6 +13,16 @@ import (
 )
 
 func main() {
+	// 変数が明示的に初期化されない場合には、暗黙的に変数の型に対するゼロ値に初期化される
+	// GOのゼロ値について: https://qiita.com/tenntenn/items/c55095585af64ca28ab5
+	// int,int8,int16,int32,int64: 0
+	// uint,uint8,uint16,uint32,uint64: 0
+	// byte,rune,uintptr: 0
+	// float32,float64: 0
+	// complex64,complex128: 0
+	// string: ""(空文字列)
+	// bool: false
+	// error: nil
 	var s, sep string
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
