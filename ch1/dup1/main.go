defdfc88f59e420c4c01c4d15d68d48d2275e0a5
fqqk <fqqk.work68@gmail.com>
2024-04-07 10:13:36 +0900

    output

--- ch1/dup1/main.go
+++ ch1/dup1/main.go
@@ -16,8 +16,12 @@ import (
 
 func main() {
 	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
+	// [memo]NewScanner スキャナを新しく作成
+	input := bufio.NewScanner(os.Stdin) // [memo]os.Stdin: ユーザーからのキーボード入力や、他のプログラムからのパイプ入力を読み取ることができます。
+	// [memo]Scanメソッドは、次のトークン(行)が存在する場合にtrueを返し、存在しない場合にfalseを返します。Textメソッドは、現在のトークンを文字列として返します。
 	for input.Scan() {
+		// ゼロ値とは、Go言語で変数が宣言されたときに自動的に割り当てられるデフォルトの値のことを指します。ゼロ値は、変数の型によって異なります。
+		// mapが指定されたキーがなくてもその型のゼロ値を返すので、初期化が不要
 		counts[input.Text()]++
 	}
 	// NOTE: ignoring potential errors from input.Err()
